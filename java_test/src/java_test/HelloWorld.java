5ef20888dbcc3c93c86c31f0bdd79b9e586fca52
matsuda1991528 <matsuda1991528@yahoo.co.jp>
2014-08-06 14:41:17 +0900

    メッセージ変更java -> test!!

--- java_test/src/java_test/HelloWorld.java
+++ java_test/src/java_test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("javaプログラム");
-		System.out.println("javaプログラム");
+		System.out.println("test!javaプログラム");
 	}
 
 }

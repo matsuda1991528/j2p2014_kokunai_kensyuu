985026d6deff5e09ca5bee3ebc0cd8bcc466cf9d
matsuda1991528 <matsuda1991528@yahoo.co.jp>
2014-08-06 15:43:57 +0900

    画像の出力

---
+++ Aplet_test/src/Test02.java
@@ -0,0 +1,21 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Image;
+
+
+public class Test02 extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	Image img0;
+	public void init(){
+		img0 = getImage(getDocumentBase(), "img/0.png");
+	}
+	public void paint(Graphics g) {
+		g.drawImage(img0, 0, 0, this);
+	}
+
+}

18f5e2475191e06d7b37f3fc5da7bd11f0e8b738
matsuda1991528 <matsuda1991528@yahoo.co.jp>
2014-08-06 15:26:11 +0900

    最初のコミット

---
+++ Aplet_test/src/Test01.java
@@ -0,0 +1,24 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class Test01 extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		//super.paint(arg0);
+		g.drawString("•¶Žš‚ð•\Ž¦", 10, 10);
+		
+	}
+
+}
